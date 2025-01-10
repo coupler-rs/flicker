6262124cad30ff120e9f5c9fcab47cab3596c031
glowcoil <micahrjohnston@gmail.com>
2025-01-09 23:00:48 -0600

    sse2 pipeline

--- src/pipeline/mod.rs
+++ src/pipeline/mod.rs
@@ -1,10 +1,22 @@
 use crate::Color;
 
-mod neon;
 mod scalar;
-
-pub use neon::Neon;
 pub use scalar::Scalar;
+
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+#[cfg(target_feature = "sse2")]
+mod sse2;
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+#[cfg(target_feature = "sse2")]
+pub use sse2::Sse2;
+
+#[cfg(all(target_arch = "aarch64", target_feature = "neon"))]
+mod neon;
+#[cfg(all(target_arch = "aarch64", target_feature = "neon"))]
+pub use neon::Neon;
+
+#[cfg(all(target_arch = "aarch64", target_feature = "neon"))]
+pub use neon::Neon;
 
 pub trait Pipeline {
     fn build(color: Color) -> Self;
--- src/raster.rs
+++ src/raster.rs
@@ -1,8 +1,15 @@
 use std::mem;
 
 use crate::flatten::Line;
-use crate::pipeline::{Neon, Pipeline, Scalar};
+use crate::pipeline::{Pipeline, Scalar};
 use crate::{geom::Point, Color};
+
+#[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+#[cfg(target_feature = "sse2")]
+use crate::pipeline::Sse2;
+
+#[cfg(all(target_arch = "aarch64", target_feature = "neon"))]
+use crate::pipeline::Neon;
 
 const BITS_PER_BITMASK: usize = u64::BITS as usize;
 const BITS_PER_BITMASK_SHIFT: usize = BITS_PER_BITMASK.trailing_zeros() as usize;
@@ -348,17 +355,17 @@ impl Rasterizer {
     }
 
     pub fn composite(&mut self, color: Color, data: &mut [u32], stride: usize) {
-        // #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
-        // {
-        //     #[cfg(target_feature = "avx2")]
-        //     return self.composite_inner::<Avx2>(color, data, stride);
+        #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
+        {
+            //     #[cfg(target_feature = "avx2")]
+            //     return self.composite_inner::<Avx2>(color, data, stride);
 
-        //     #[cfg(all(not(target_feature = "avx2"), target_feature = "sse2"))]
-        //     return self.composite_inner::<Sse2>(color, data, stride);
+            #[cfg(all(not(target_feature = "avx2"), target_feature = "sse2"))]
+            return self.composite_inner::<Sse2>(color, data, stride);
 
-        //     #[cfg(not(any(target_feature = "avx2", target_feature = "sse2")))]
-        //     return self.composite_inner::<Scalar>(color, data, stride);
-        // }
+            #[cfg(not(any(target_feature = "avx2", target_feature = "sse2")))]
+            return self.composite_inner::<Scalar>(color, data, stride);
+        }
 
         #[cfg(target_arch = "aarch64")]
         {
